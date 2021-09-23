7afd6b1947a05e25e3a952a04bb6566ff3c58bb0
Bonamente <56506035@protonmail.com>
2021-09-23 10:24:02 +0500

    fix: implement correct slide number display

--- museum/js/welcome-swiper.js
+++ museum/js/welcome-swiper.js
@@ -1,13 +1,25 @@
-var swiper = new Swiper(".mySwiper", {
+const swiper = new Swiper('.welcome__swiper', {
   slidesPerView: 1,
-  spaceBetween: 30,
+  spaceBetween: 0,
+  speed: 1000,
   loop: true,
+
   pagination: {
-    el: ".swiper-pagination",
+    el: '.swiper-pagination',
     clickable: true,
   },
+
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
+});
+
+const currentNumber = document.querySelector('.current-slide-number');
+const totalNumber = document.querySelector('.total-number');
+
+totalNumber.textContent = `0${swiper.slides.length - 2}`
+
+swiper.on('slideChange', () => {  
+  currentNumber.textContent = `0${swiper.realIndex + 1}`;
 });
