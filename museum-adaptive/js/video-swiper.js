8a59e1207d60bed4f776f98e270de5e95df6d35f
Bonamente <56506035@protonmail.com>
2021-10-02 21:54:40 +0500

    feat: add the rest of the styles for a resolution of 768px

--- museum-adaptive/js/video-swiper.js
+++ museum-adaptive/js/video-swiper.js
@@ -1,8 +1,16 @@
 const swiper2 = new Swiper('.video__swiper', {
-  slidesPerView: 3,
-  spaceBetween: 42,
+  slidesPerView: 2,
+  spaceBetween: 20,
   speed: 1000,
   loop: true,
+
+  breakpoints: {
+    // when window width is >= 769px
+    769: {
+      slidesPerView: 3,
+      spaceBetween: 42
+    }    
+  },
 
   pagination: {
     el: '.video__swiper-pagination',
