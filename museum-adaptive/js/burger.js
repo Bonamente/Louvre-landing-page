e46100ce292d481623d73503ddded7289c4aacf2
Bonamente <56506035@protonmail.com>
2021-10-03 22:54:19 +0500

    feat: add styles for 440px resolution

--- museum-adaptive/js/burger.js
+++ museum-adaptive/js/burger.js
@@ -5,6 +5,7 @@ const navToggle = document.querySelector('.nav-toggle');
 const welcomeTitle = document.querySelector('.welcome__title');
 const welcomeText = document.querySelector('.welcome__text');
 const welcomeLink = document.querySelector('.welcome__link');
+const burgerMenuLinks = document.querySelectorAll('.main-nav__item > a');
 
 navToggle.addEventListener('click', () => {
   if (navMain.classList.contains('main-nav--closed')) {
@@ -27,3 +28,16 @@ navToggle.addEventListener('click', () => {
     welcomeLink.style.opacity = 1;
   }
 });
+
+[...burgerMenuLinks].forEach((link) => {
+  link.addEventListener('click', (e) => {
+    navToggle.setAttribute("aria-expanded", false);
+    pageHeader.classList.add('nav-closed');
+    pageHeader.classList.remove('nav-opened');
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened')
+    welcomeTitle.style.opacity = 1;
+    welcomeText.style.opacity = 1;
+    welcomeLink.style.opacity = 1;
+  })
+})
