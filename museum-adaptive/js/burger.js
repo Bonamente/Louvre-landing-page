e19a819fc8bd3885a3d2840ac72b5d140541ce60
Bonamente <56506035@protonmail.com>
2021-10-04 16:22:16 +0500

    feat: add self-check

--- museum-adaptive/js/burger.js
+++ museum-adaptive/js/burger.js
@@ -1,4 +1,3 @@
-
 const pageHeader = document.querySelector('.page-header__container');
 const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.nav-toggle');
@@ -7,37 +6,39 @@ const welcomeText = document.querySelector('.welcome__text');
 const welcomeLink = document.querySelector('.welcome__link');
 const burgerMenuLinks = document.querySelectorAll('.main-nav__item > a');
 
+const openMenu = () => {
+  navToggle.setAttribute("aria-expanded", true);
+  pageHeader.classList.remove('nav-closed');
+  pageHeader.classList.add('nav-opened');
+  navMain.classList.remove('main-nav--closed');
+  navMain.classList.add('main-nav--opened');
+  welcomeTitle.style.opacity = 0;
+  welcomeText.style.opacity = 0;
+  welcomeLink.style.opacity = 0;
+};
+
+const closeMenu = () => {
+  navToggle.setAttribute("aria-expanded", false);
+  pageHeader.classList.add('nav-closed');
+  pageHeader.classList.remove('nav-opened');
+  navMain.classList.add('main-nav--closed');
+  navMain.classList.remove('main-nav--opened')
+  welcomeTitle.style.opacity = 1;
+  welcomeText.style.opacity = 1;
+  welcomeLink.style.opacity = 1;
+}
+
+document.addEventListener('click', (e) => { 
+  if (e.target.closest('.main-nav')) return;
+  if (navToggle !== e.target) closeMenu();
+});
+
 navToggle.addEventListener('click', () => {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navToggle.setAttribute("aria-expanded", true);
-    pageHeader.classList.remove('nav-closed');
-    pageHeader.classList.add('nav-opened');
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-    welcomeTitle.style.opacity = 0;
-    welcomeText.style.opacity = 0;
-    welcomeLink.style.opacity = 0;
-  } else {
-    navToggle.setAttribute("aria-expanded", false);
-    pageHeader.classList.add('nav-closed');
-    pageHeader.classList.remove('nav-opened');
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened')
-    welcomeTitle.style.opacity = 1;
-    welcomeText.style.opacity = 1;
-    welcomeLink.style.opacity = 1;
-  }
+  navMain.classList.contains('main-nav--closed') 
+    ? openMenu()
+    : closeMenu();
 });
 
 [...burgerMenuLinks].forEach((link) => {
-  link.addEventListener('click', (e) => {
-    navToggle.setAttribute("aria-expanded", false);
-    pageHeader.classList.add('nav-closed');
-    pageHeader.classList.remove('nav-opened');
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened')
-    welcomeTitle.style.opacity = 1;
-    welcomeText.style.opacity = 1;
-    welcomeLink.style.opacity = 1;
-  })
-})
+  link.addEventListener('click', closeMenu)
+});
