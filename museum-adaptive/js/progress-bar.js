b967617dc2273dc60bd3ed2ff4696c3e58a0c958
Bonamente <56506035@protonmail.com>
2021-10-04 13:43:48 +0500

    feat: add optimization for video & images

--- museum-adaptive/js/progress-bar.js
+++ museum-adaptive/js/progress-bar.js
@@ -2,7 +2,7 @@ const progressVideo = document.querySelector('.video__progress--video');
 const progressVolume = document.querySelector('.video__progress--volume');
 
 const handleInput = function() {
-  const value = (this.value-this.min)/(this.max-this.min)*100;
+  const value = (this.value - this.min) / (this.max - this.min)*100;
 
   this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #C4C4C4 ${value}%, #C4C4C4 100%)`;
 };
